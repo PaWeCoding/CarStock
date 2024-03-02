5a4974faf5d3ef1db1b8a781548cee23335b989e
Patrick <patrick_weibel@gmx.ch>
2024-03-02 08:58:41 +0100

    Changed to "normal" List type

--- Infrastructure/Car.Infrastructure/Repositories/CarRepository.cs
+++ Infrastructure/Car.Infrastructure/Repositories/CarRepository.cs
@@ -1,12 +1,11 @@
 ﻿using Car.Infrastructure.Abstractions.Entities;
 using Car.Infrastructure.Abstractions.Repositories;
-using System.Collections.Immutable;
 
 namespace Car.Infrastructure.Repositories
 {
     public sealed class CarRepository : ICarRepository
     {
-        private readonly IImmutableList<CarBase> _cars = [];
+        private readonly List<CarBase> _cars = [];
 
         public void InsertMany(IEnumerable<CarBase> cars) =>
             _cars.AddRange(cars);
