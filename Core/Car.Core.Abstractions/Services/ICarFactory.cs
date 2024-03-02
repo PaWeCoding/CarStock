731ad6ddcd380ef0b442b21a8c728a5924dc6536
Patrick <patrick_weibel@gmx.ch>
2024-03-02 08:27:55 +0100

    Factory implemented to create Ford and VW cars

---
+++ Core/Car.Core.Abstractions/Services/ICarFactory.cs
@@ -0,0 +1,11 @@
+﻿using Car.Core.Abstractions.Config;
+using Car.Infrastructure.Abstractions.Entities;
+
+namespace Car.Core.Abstractions.Services
+{
+    public interface ICarFactory
+    {
+        FordCar CreateFord(Action<CarConfig> config);
+        VWCar CreateVW(Action<CarConfig> config);
+    }
+}
