af5ef17a7f657f877804864db6b1bed993695412
Patrick <patrick_weibel@gmx.ch>
2024-03-02 16:21:45 +0100

    Test added for CarStockService

---
+++ Core/Car.Core.Tests/Services/CarStockServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Car.Core.Services;
+using Car.Infrastructure.Abstractions.Entities;
+using Car.Infrastructure.Abstractions.Repositories;
+using NSubstitute;
+
+namespace Car.Core.Tests.Services
+{
+    [TestClass]
+    public class CarStockServiceTests
+    {
+        private ICarRepository _carRepository = null!;
+        private CarStockService _testee = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _carRepository = Substitute.For<ICarRepository>();
+            _testee = new CarStockService(_carRepository);
+        }
+
+        [TestMethod]
+        public void GetStockOrderedByYearDesc_InsertCarsWithDifferentYears_ReturnsCarsOrderedByYearDesc()
+        {
+            // Arrange
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            var cars = new List<CarBase>
+            {
+                new FordCar(2018, default, default, default, default),
+                new FordCar(2024, default, default, default, default)
+            };
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+            _carRepository.GetAll().Returns(cars.AsQueryable());
+
+            // Act
+            var carStock = _testee.GetStockOrderedByYearDesc();
+
+            // Assert
+            var firstCar = carStock.First();
+            var lastCar = carStock.Last();
+            Assert.AreEqual(2024, firstCar.Year);
+            Assert.AreEqual(2018, lastCar.Year);
+        }
+    }
+}
