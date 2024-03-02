9db8b2bcb19d798265e1c219534422029ce3b869
Patrick <patrick_weibel@gmx.ch>
2024-03-02 07:59:44 +0100

    Repository to persist instances of type Car implemented

---
+++ Infrastructure/Car.Infrastructure/Repositories/CarRepository.cs
@@ -0,0 +1,16 @@
+﻿using Car.Infrastructure.Abstractions.Entities;
+using Car.Infrastructure.Abstractions.Repositories;
+using System.Collections.Immutable;
+
+namespace Car.Infrastructure.Repositories
+{
+    public sealed class CarRepository : ICarRepository
+    {
+        private readonly IImmutableList<CarBase> _cars = [];
+
+        public void InsertMany(IEnumerable<CarBase> cars) =>
+            _cars.AddRange(cars);
+
+        public IQueryable<CarBase> GetAll() => _cars.AsQueryable();
+    }
+}
